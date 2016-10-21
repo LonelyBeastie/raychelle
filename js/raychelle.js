7f974966dcc3b8ef21f634bac220a5f4f9721c47
Jake <jacobsiddall@gmail.com>
2016-10-21 16:17:42 -0500

    starts the big menu bar

--- js/raychelle.js
+++ js/raychelle.js
@@ -1,3 +1,10 @@
 $(document).scroll(function() {
   $('.gm-nav a').smoothScroll();
+
+  var y = $(this).scrollTop();
+  if (y > 35) {
+    $('.navbar').fadeIn();
+  } else {
+    $('.navbar').fadeOut();
+  }
 });
