ad6c36d2bfc6ce9aa85e8b1989f08744bfe7080a
Jake <jacobsiddall@gmail.com>
2016-10-24 09:57:12 -0500

    puts in big nav (and makes it reform to the little one on window resizing)

--- js/raychelle.js
+++ js/raychelle.js
@@ -1,10 +1,26 @@
-$(document).scroll(function() {
+$(document).ready(function() {
   $('.gm-nav a').smoothScroll();
+  if ($(window).width()<992){
+    $('.navbar').fadeIn();
+  }
+});
 
-  var y = $(this).scrollTop();
-  if (y > 35) {
+$(window).resize(function() {
+  var x = $(this).width();
+  if (x < 992) {
     $('.navbar').fadeIn();
   } else {
     $('.navbar').fadeOut();
+  }
+});
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if ($(window).width()>992){
+    if (y > 35) {
+      $('.navbar').fadeIn();
+    } else {
+      $('.navbar').fadeOut();
+    }
   }
 });
